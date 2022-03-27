61952c6b7a4231c826d9820615b6a021332449e0
Souvik Biswas <sbis1999@gmail.com>
2022-03-27 16:52:32 +0530

    Update upload file page

--- lib/screens/transcription/upload_page.dart
+++ lib/screens/transcription/upload_page.dart
@@ -1,17 +1,17 @@
 import 'dart:developer';
 import 'dart:io';
 
-import 'package:cloud_functions/cloud_functions.dart';
 import 'package:deepgram_transcribe/res/custom_colors.dart';
 import 'package:deepgram_transcribe/screens/transcription/transcription_page.dart';
-import 'package:deepgram_transcribe/utils/helper.dart';
-import 'package:deepgram_transcribe/utils/storage_client.dart';
 import 'package:deepgram_transcribe/widgets/wave_visualizer.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:subtitle/subtitle.dart';
+import 'package:tuple/tuple.dart';
+
+import '../../widgets/bottom_sheet_widget.dart';
 
 class UploadPage extends StatefulWidget {
   const UploadPage({Key? key}) : super(key: key);
@@ -38,7 +38,6 @@ class _UploadPageState extends State<UploadPage> {
     // });
 
     if (file != null) {
-      log('${file.files.single.name}');
       return file.files.single;
     }
 
@@ -101,9 +100,12 @@ class _UploadPageState extends State<UploadPage> {
                         ),
                       ),
                       onPressed: () async {
-                        final file = await pickFile();
-                        if (file != null && file.path != null) {
-                          final List<Subtitle> subtitles =
+                        final pickedFile = await pickFile();
+
+                        if (pickedFile != null && pickedFile.path != null) {
+                          final file = File(pickedFile.path!);
+
+                          final Tuple2<List<Subtitle>, String> result =
                               await showModalBottomSheet(
                             isDismissible: false,
                             context: context,
@@ -114,6 +116,9 @@ class _UploadPageState extends State<UploadPage> {
                             },
                           );
 
+                          final subtitles = result.item1;
+                          final docId = result.item2;
+
                           log('Received transcripts!');
 
                           Navigator.of(context).pushReplacement(
@@ -121,6 +126,7 @@ class _UploadPageState extends State<UploadPage> {
                               builder: (context) => TranscriptionPage(
                                 subtitles: subtitles,
                                 audioFile: file,
+                                docId: docId,
                               ),
                             ),
                           );
@@ -143,137 +149,6 @@ class _UploadPageState extends State<UploadPage> {
           ],
         ),
       ),
-    );
-  }
-}
-
-class BottomSheetWidget extends StatefulWidget {
-  const BottomSheetWidget({
-    Key? key,
-    required this.file,
-  }) : super(key: key);
-
-  final PlatformFile file;
-
-  @override
-  State<BottomSheetWidget> createState() => _BottomSheetWidgetState();
-}
-
-class _BottomSheetWidgetState extends State<BottomSheetWidget> {
-  late final StorageClient _storageClient;
-  late final FirebaseFunctions _functions;
-  late final PlatformFile _file;
-
-  String text = '';
-  Widget icon = const CircularProgressIndicator(
-    valueColor: AlwaysStoppedAnimation<Color>(
-      Colors.black54,
-    ),
-  );
-
-  generateTranscript() async {
-    setState(() {
-      text = 'Uploading...';
-    });
-    final downloadUrl = await _storageClient.uploadRecording(
-      fileName: _file.name,
-      audioFile: File(_file.path!),
-    );
-    log('URL: $downloadUrl');
-    setState(() {
-      text = 'Generating transcripts...';
-    });
-
-    final callable = _functions.httpsCallable('getTranscription');
-    final resp = await callable.call(<String, dynamic>{
-      'url': downloadUrl,
-    });
-
-    // debugPrint("result: ${resp.data}");
-
-    final String data = resp.data;
-    final subtitles = await Helper.getSubtitle(data);
-
-    setState(() {
-      text = 'Generated successfully';
-      icon = const Icon(
-        Icons.check,
-        color: Colors.black54,
-        size: 36,
-      );
-    });
-
-    await Future.delayed(const Duration(seconds: 2));
-
-    Navigator.of(context).pop(subtitles);
-  }
-
-  @override
-  void initState() {
-    _storageClient = StorageClient();
-    _file = widget.file;
-    _functions = FirebaseFunctions.instance;
-    generateTranscript();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          color: Colors.red.shade600,
-          width: double.maxFinite,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16.0,
-              vertical: 6.0,
-            ),
-            child: Row(
-              children: const [
-                Icon(
-                  Icons.warning_amber_rounded,
-                  color: Colors.white,
-                ),
-                SizedBox(width: 8),
-                Text(
-                  'Please do not close the app.',
-                  style: TextStyle(
-                    fontSize: 14,
-                    // fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        Container(
-          color: CustomColors.green,
-          width: double.maxFinite,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16.0,
-              vertical: 16.0,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  text,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
-                    color: CustomColors.black,
-                  ),
-                ),
-                icon,
-              ],
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
