0739df1e0bb6eb9afb27beb7edcd9c5c5006e121
Souvik Biswas <sbis1999@gmail.com>
2022-03-29 21:59:39 +0530

    Update cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,6 @@
 const functions = require("firebase-functions");
 const {Deepgram} = require("@deepgram/sdk");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 exports.getTranscription = functions.https.onCall(async (data, context) => {
   try {
     const deepgram = new Deepgram(process.env.DEEPGRAM_API_KEY);
@@ -19,12 +11,26 @@ exports.getTranscription = functions.https.onCall(async (data, context) => {
     const response = await deepgram.transcription.preRecorded(audioSource, {
       punctuate: true,
       utterances: true,
-      // other options are available
     });
+
+    console.log(response.results.utterances.length);
+
+    const confidenceList = [];
+    for (let i =0; i < response.results.utterances.length; i++) {
+      confidenceList.push(response.results.utterances[i].confidence);
+    }
 
     const webvttTranscript = response.toWebVTT();
 
-    return webvttTranscript;
+    const finalTranscript = {
+      transcript: webvttTranscript,
+      confidences: confidenceList,
+    };
+
+    const finalTranscriptJSON = JSON.stringify(finalTranscript);
+    console.log(finalTranscriptJSON);
+
+    return finalTranscriptJSON;
   } catch (error) {
     console.error(`Unable to transcribe. Error ${error}`);
     throw new functions.https.HttpsError("aborted", "Could not transcribe");
