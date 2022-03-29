907e38ac9c4f38c440626b19964ff1034829dd30
Souvik Biswas <sbis1999@gmail.com>
2022-03-29 21:58:58 +0530

    Remove unused code from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,9 @@
-import 'dart:developer';
-import 'dart:io';
-import 'dart:typed_data';
-
-import 'package:cloud_functions/cloud_functions.dart';
 import 'package:deepgram_transcribe/firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:mime/mime.dart';
-import 'package:subtitle/subtitle.dart';
 
+import 'res/custom_colors.dart';
 import 'screens/authentication/login_page.dart';
 import 'screens/dashboard_page.dart';
 
@@ -32,100 +25,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Poppins',
-        primarySwatch: Colors.blue,
+        primarySwatch: primaryBlack,
       ),
       // home: const HomePage(),
       home: FirebaseAuth.instance.currentUser == null
           ? const LoginPage()
           : const DashboardPage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final String audioAsset = "assets/sample_audio.mp3";
-  final functions = FirebaseFunctions.instance;
-  String finalText = '';
-
-  Future<dynamic> getAudioBytes() async {
-    ByteData bytes = await rootBundle.load(audioAsset);
-    return bytes;
-  }
-
-  Future<String> sendData() async {
-    HttpsCallable callable = functions.httpsCallable('getTranscription');
-
-    final resp = await callable();
-
-    print("result: ${resp.data}");
-
-    return resp.data;
-  }
-
-  Future<List<Subtitle>> getSubtitle(String vttData) async {
-    var controller = SubtitleController(
-      provider: SubtitleProvider.fromString(
-        data: vttData,
-        type: SubtitleType.vtt,
-      ),
-    );
-    await controller.initial();
-    final subtitles = controller.subtitles;
-
-    printResult(subtitles);
-    return subtitles;
-  }
-
-  void printResult(List<Subtitle> subtitles) {
-    // subtitles.sort((s1, s2) => s1.compareTo(s2));
-    for (var result in subtitles) {
-      print(
-        '(${result.index}) Start: ${result.start}, end: ${result.end} [${result.data}]',
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(),
-          ElevatedButton(
-            onPressed: () async {
-              final buffer = await getAudioBytes();
-              final mime = lookupMimeType(audioAsset);
-
-              log('BUFFER: ${buffer}');
-              log('MIME: $mime');
-
-              final data = await sendData();
-              final subtitles = await getSubtitle(data);
-
-              String parsedText = '';
-
-              subtitles.forEach((element) {
-                parsedText += element.data.substring(1);
-              });
-              print(parsedText);
-              setState(() {
-                finalText = parsedText;
-              });
-            },
-            child: const Text('Get transcribe'),
-          ),
-          const SizedBox(height: 24),
-          Text(finalText),
-        ],
-      ),
     );
   }
 }
