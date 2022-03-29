e0b2a97e00ad2bf5f198877fd3f6a7c2d20f0612
Souvik Biswas <sbis1999@gmail.com>
2022-03-29 21:59:56 +0530

    Add black color as material color

--- lib/res/custom_colors.dart
+++ lib/res/custom_colors.dart
@@ -6,3 +6,20 @@ class CustomColors {
   static const Color black = Color(0xFF2F2F2F);
   static const Color green = Color(0xFFCBEF43);
 }
+
+const MaterialColor primaryBlack = MaterialColor(
+  _blackPrimaryValue,
+  <int, Color>{
+    50: Color(0xFF000000),
+    100: Color(0xFF000000),
+    200: Color(0xFF000000),
+    300: Color(0xFF000000),
+    400: Color(0xFF000000),
+    500: Color(_blackPrimaryValue),
+    600: Color(0xFF000000),
+    700: Color(0xFF000000),
+    800: Color(0xFF000000),
+    900: Color(0xFF000000),
+  },
+);
+const int _blackPrimaryValue = 0xFF000000;
